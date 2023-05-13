524a9b9d684c31c65918cf2f44d7cbd2939488a7
ManOfTheFuture <takacsdavid2003@gmail.com>
2023-05-13 10:48:52 +0000

    fix order model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,21 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
-    use HasFactory;
-
     protected $table = "orders";
 
     protected $fillable = [
-        'id',
-        'userid',
         'status',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'userid', 'id');
+    }
 
     public function foods(): BelongsToMany
     {
