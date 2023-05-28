4df57eb40d895aa992d04a077309037903a9ba28
ManOfTheFuture <takacsdavid2003@gmail.com>
2023-05-28 17:17:34 +0000

    add classnames to top

--- resources/js/Components/PaddedSection.jsx
+++ resources/js/Components/PaddedSection.jsx
@@ -1,6 +1,6 @@
-export default function PaddedSection({ children, ...props }) {
+export default function PaddedSection({ children, className, ...props }) {
     return (
-        <section className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <section className={`bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg ${className}`}>
             <div className="p-3 text-gray-900 dark:text-gray-100">
                 <div {...props}>
                     {children}
