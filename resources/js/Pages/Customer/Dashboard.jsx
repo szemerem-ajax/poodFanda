7bc95ec0d5684de3e08f6a422a25f367f098d25a
ManOfTheFuture <takacsdavid2003@gmail.com>
2023-05-28 17:17:50 +0000

    implement food filters for customer

--- resources/js/Pages/Customer/Dashboard.jsx
+++ resources/js/Pages/Customer/Dashboard.jsx
@@ -3,17 +3,55 @@ import { Head } from '@inertiajs/react';
 import { createContext, useEffect, useState } from 'react';
 import FoodView from './Partials/FoodView';
 import Basket from './Partials/Basket';
+import InputLabel from '@/Components/InputLabel';
+import TextInput from '@/Components/TextInput';
+import SelectInput from '@/Components/SelectInput';
 
 export const BasketContext = createContext([null, null]);
 
+function useDebounce(value, delay) {
+    // State and setters for debounced value
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(
+      () => {
+        // Update debounced value after delay
+        const handler = setTimeout(() => {
+          setDebouncedValue(value);
+        }, delay);
+        // Cancel the timeout if value changes (also on delay change or unmount)
+        // This is how we prevent debounced value from updating if value is changed ...
+        // .. within the delay period. Timeout gets cleared and restarted.
+        return () => {
+          clearTimeout(handler);
+        };
+      },
+      [value, delay] // Only re-call effect if value or delay changes
+    );
+    return debouncedValue;
+  }
+
 export default function Dashboard({ auth }) {
+    const [filters, setFilters] = useState({
+        name: '',
+        restaurant: '',
+        category: '',
+        min: '',
+        max: ''
+    });
+    const [categories, setCategories] = useState([]);
     const [basket, setBasket] = useState([]);
     const [foods, setFoods] = useState([]);
+    const debouncedFilters = useDebounce(filters, 500);
 
     useEffect(() => {
-        axios.get(route('foods.index'))
-            .then(r => setFoods(r.data.data));
+        axios.get(route('categories.index'))
+            .then(r => setCategories(['', ...r.data.data.map(c => c.name)]))
     }, []);
+
+    useEffect(() => {
+        axios.get(route('foods.index', debouncedFilters))
+            .then(r => setFoods(r.data.data));
+    }, [debouncedFilters]);
 
     return (
         <AuthenticatedLayout
@@ -23,6 +61,29 @@ export default function Dashboard({ auth }) {
             <Head title="Dashboard" />
 
             <h1 className='text-2xl font-semibold mb-8'>Welcome to poodFanda {auth.user.name}!</h1>
+
+            <div className="flex flex-col lg:flex-row flex-wrap lg:flex-nowrap gap-1 mb-4">
+                <div>
+                    <InputLabel htmlFor='name' value='Name' />
+                    <TextInput id='name' type='text' name='name' value={filters.name} onChange={e => setFilters(p => ({ ...p, name: e.target.value }))} />
+                </div>
+                <div>
+                    <InputLabel htmlFor='restaurant' value='Restaurant' />
+                    <TextInput id='restaurant' type='text' name='restaurant' value={filters.restaurant} onChange={e => setFilters(p => ({ ...p, restaurant: e.target.value }))} />
+                </div>
+                <div>
+                    <InputLabel htmlFor='category' value='Category' />
+                    <SelectInput id='category' options={categories} value={filters.category} onChange={e => setFilters(p => ({ ...p, category: e.target.value }))} />
+                </div>
+                <div>
+                    <InputLabel htmlFor='min' value='Price from' />
+                    <TextInput id='min' type='number' value={filters.min} onChange={e => setFilters(p => ({ ...p, min: Number(e.target.value) }))} />
+                </div>
+                <div>
+                    <InputLabel htmlFor='max' value='Price to' />
+                    <TextInput id='max' type='number' value={filters.max} onChange={e => setFilters(p => ({ ...p, max: Number(e.target.value) }))} />
+                </div>
+            </div>
 
             <BasketContext.Provider value={[basket, setBasket]}>
                 <div className="flex flex-nowrap overflow-x-scroll md:overflow-hidden md:flex-wrap gap-2">
